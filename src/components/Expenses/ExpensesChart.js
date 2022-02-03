01e000aba1f2aef933ff1dc5ea25fbc00aa4b3af
luwa-star <mercyademiju24@gmail.com>
2022-02-03 08:16:55 +0100

    project update1

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Chart from '../Chart/Chart';
 
 const ExpensesChart = props =>{
-    {/*Set up the datapoints prop*/}
+    
     const chartDataPoints =[
         {label: 'Jan', value:0},
         {label: 'Feb', value:0},
